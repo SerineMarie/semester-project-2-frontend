e1dd476312585059174a47d81bd1334ac98a14a7
SerineMarie <71319320+SerineMarie@users.noreply.github.com>
2022-06-11 14:34:41 +0200

    update

--- js/components/home/indexHtml.js
+++ js/components/home/indexHtml.js
@@ -7,8 +7,8 @@ export async function heroBanner(){
     try{
         const rep = await fetch(heroBannerUrl);
         const data = await rep.json();
-        const heroBannerImg = "https://semester-project-2-serinemarie.herokuapp.com/home" + data.hero_banner.url;
-        const heroBannerResponsive = "https://semester-project-2-serinemarie.herokuapp.com/home" + data.hero_banner.url;        
+        const heroBannerImg = "https://semester-project-2-serinemarie.herokuapp.com" + data.hero_banner.url;
+        const heroBannerResponsive = "https://semester-project-2-serinemarie.herokuapp.com" + data.hero_banner.url;        
 
         bannerContainer.innerHTML += `<div class="banner_container-img" style="background-image: url('${heroBannerImg}');"></div>
                                         <div class="banner_container-responsive-img"><img src="${heroBannerResponsive}"/></div>`
